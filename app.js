467dfe33a42b3929b3cc39adc940fe72bdb3c66c
lonk <rinke2005@gmail.com>
2014-10-28 00:13:25 +0100

    Commit 2, jeu presque fonctionnel

--- app.js
+++ app.js
@@ -48,8 +48,25 @@ io.sockets.on('connection', function (socket) {
 		}
 	});
 
-	socket.on('speak', function(data) {
+	socket.on('speak', function (data) {
 		roomRequest(myRoom, 'speak', {message:data.message, pseudo:myRoom.getPlayer(socket).getInfos().pseudo, color:myRoom.getPlayer(socket).getColor() });
+	});
+
+	socket.on('killbox', function (data) {
+		roomRequest(myRoom, 'killbox', data);
+		myRoom.getBox(data.id).kill();
+
+		if(myRoom.getBox(data.id).getColor() == myRoom.getPlayer(socket).getColor()) {
+			myRoom.getPlayer(socket).addPoints(1);
+		} else if(myRoom.getBox(data.id).getColor() == myRoom.getMalus()) {
+			myRoom.getPlayer(socket).rmPoints(3);
+			if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
+		} else if(myRoom.getBox(data.id).getColor() == myRoom.getBonus()) {
+			// JAY PAS ENCORE FAY LES POUVOIRS PASKE CAY LONG À FAYRE
+		} else {
+			myRoom.getPlayer(socket).rmPoints(2);
+			if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
+		}
 	});
 });
 
@@ -59,8 +76,48 @@ function launchRoom(room) {
 		objectPlayer = {pseudo: room.getPlayers()[player].getInfos().pseudo, color: room.getPlayers()[player].getColor()};
 		playersInfos.push(objectPlayer);
 	}
-	console.log(playersInfos);
 	roomRequest(room, 'start', playersInfos);
+
+	setTimeout(function() { launchGame(room); }, 10000);
+}
+
+function launchGame(room) {
+	stepGame(room);
+}
+
+function stepGame(room) {
+	var blackRandom = Math.floor(Math.random() * 20);
+	if(blackRandom == 0 && room.getNbBlack() < 5) var stepColor = room.getMalus();
+	else {
+		var stepColor = room.getCurrentColor();
+		room.nextColor();
+	}
+
+	generateBox(room, stepColor);
+
+	if(room.getTime()+2*room.getRatio() >= 30) {
+		setTimeout(function() { endGame(room); }, 30-room.getTime());
+		room.setTime(30);
+	} else {
+		setTimeout(function() { stepGame(room); }, 2*room.getRatio()*1000);
+		room.setTime(room.getTime()+2*room.getRatio());
+		room.nextRatio();
+	}
+}
+
+function endGame(room) {
+	var points = [];
+	for(player in room.getPlayers()) {
+		points.push({pseudo: room.getPlayers()[player].getInfos().pseudo, points: room.getPlayers()[player].getPoints()});
+	}
+	roomRequest(room, 'stop', points);
+}
+
+function generateBox(room, stepColor) {
+	var box = new boxes.Box(room.nextBox(), stepColor);
+	room.addBox(box);
+	roomRequest(room, 'box', {box: box});
+	//console.log(box);
 }
 
 function roomRequest(room, type, data) {
