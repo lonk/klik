ff94e830a0199343c4f17bf1ae54fe7fba924d5e
lonk <rinke2005@gmail.com>
2014-11-02 16:56:21 +0100

    Ce commit vous est offert par la société caribou !

--- app.js
+++ app.js
@@ -34,10 +34,24 @@ io.sockets.on('connection', function (socket) {
 
 			if(currentRoom.isFull()) { 
 				currentRoom = new rooms.Room(roomSize);
-				roomRequest(myRoom, 'ready', {});
+				myRoom.roomRequest('ready', {});
+			} else {
+				var roomToLoad = currentRoom;
+				if(roomToLoad.getPowerList().length == 0) {
+					connection.query("SELECT * FROM powers", function(err, rows, fields) {
+						if (err) throw err;
+						var powers = [];
+						for(power in rows) {
+							for(i=0;i<rows[power].weight;i++) {
+								powers.push(rows[power].id);
+							}
+						}
+						roomToLoad.setPowerList(powers);
+					});
+				}
 			}
-		});
 
+		});
 	});
 
 	socket.on('ready', function (data) {
@@ -49,23 +63,33 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('speak', function (data) {
-		roomRequest(myRoom, 'speak', {message:data.message, pseudo:myRoom.getPlayer(socket).getInfos().pseudo, color:myRoom.getPlayer(socket).getColor() });
+		myRoom.roomRequest('speak', {message:data.message, pseudo:myRoom.getPlayer(socket).getInfos().pseudo, color:myRoom.getPlayer(socket).getColor() });
 	});
 
 	socket.on('killbox', function (data) {
-		roomRequest(myRoom, 'killbox', data);
-		myRoom.getBox(data.id).kill();
+		if(!myRoom.getBox(data.id).isKilled()) {
+			if(myRoom.getBox(data.id).getColor() == myRoom.getPlayer(socket).getColor()) {
+				myRoom.getPlayer(socket).addPoints(1);
+			} else if(myRoom.getBox(data.id).getColor() == myRoom.getMalus()) {
+				myRoom.getPlayer(socket).rmPoints(3);
+				if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
+			} else if(myRoom.getBox(data.id).getColor() == myRoom.getBonus()) {
+				myRoom.getPlayer(socket).givePower(myRoom.getBox(data.id).getPower());
+				myRoom.getPlayer(socket).send('addpower', {box: data.id, power: myRoom.getBox(data.id).getPower()});
+			} else {
+				myRoom.getPlayer(socket).rmPoints(2);
+				if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
+			}
 
-		if(myRoom.getBox(data.id).getColor() == myRoom.getPlayer(socket).getColor()) {
-			myRoom.getPlayer(socket).addPoints(1);
-		} else if(myRoom.getBox(data.id).getColor() == myRoom.getMalus()) {
-			myRoom.getPlayer(socket).rmPoints(3);
-			if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
-		} else if(myRoom.getBox(data.id).getColor() == myRoom.getBonus()) {
-			// JAY PAS ENCORE FAY LES POUVOIRS PASKE CAY LONG À FAYRE
-		} else {
-			myRoom.getPlayer(socket).rmPoints(2);
-			if(myRoom.getPlayer(socket).getPoints() < 0) myRoom.getPlayer(socket).setPoints(0);
+			myRoom.roomRequest('killbox', data);
+			myRoom.getBox(data.id).kill();
+		}
+	});
+
+	socket.on('attack', function (data) {
+		if(myRoom.getPlayer(socket).hasPower(data.power)) {
+			myRoom.otherPlayersRequest(socket, 'attack', {power: data.power});
+			myRoom.getPlayer(socket).discardPower(data.power);
 		}
 	});
 });
@@ -76,24 +100,50 @@ function launchRoom(room) {
 		objectPlayer = {pseudo: room.getPlayers()[player].getInfos().pseudo, color: room.getPlayers()[player].getColor()};
 		playersInfos.push(objectPlayer);
 	}
-	roomRequest(room, 'start', playersInfos);
+	room.roomRequest('start', playersInfos);
 
 	setTimeout(function() { launchGame(room); }, 10000);
 }
 
 function launchGame(room) {
+	room.setPowers(generatePowers(room));
+
+	for(player in room.getPlayers()) {
+		var power = room.getOnePower();
+		room.getPlayers()[player].givePower(power);
+		room.getPlayers()[player].send('addpower', {box: 'bonus', power: power});
+	}
+
+	var timesPower = [];
+
+	for(i=0;i<room.getNbPowers();i++) {
+		timesPower.push(5+Math.floor(Math.random() * 20));
+	}
+	timesPower.sort(compare);
+	room.setTimesPower(timesPower);
+
+	console.log(room.timesPower);
+	console.log(room.getPowers());
+
 	stepGame(room);
 }
 
 function stepGame(room) {
 	var blackRandom = Math.floor(Math.random() * 20);
-	if(blackRandom == 0 && room.getNbBlack() < 5) var stepColor = room.getMalus();
+	var power = false;
+
+	if(room.getTime() >= room.getTimePower() && room.getNbPowers() > 0) {
+		var stepColor = room.getBonus();
+		power = room.getOnePower();
+		room.changeTimePower();
+	}
+	else if(blackRandom == 0 && room.getNbBlack() < 5) var stepColor = room.getMalus();
 	else {
 		var stepColor = room.getCurrentColor();
 		room.nextColor();
 	}
 
-	generateBox(room, stepColor);
+	generateBox(room, stepColor, power);
 
 	if(room.getTime()+2*room.getRatio() >= 30) {
 		setTimeout(function() { endGame(room); }, 30-room.getTime());
@@ -107,21 +157,44 @@ function stepGame(room) {
 
 function endGame(room) {
 	var points = [];
+	var winner = room.getPlayers()[0];
 	for(player in room.getPlayers()) {
-		points.push({pseudo: room.getPlayers()[player].getInfos().pseudo, points: room.getPlayers()[player].getPoints()});
+		if(room.getPlayers()[player].getPoints() > winner.getPoints()) winner = room.getPlayers()[player];
+		var noPowerState = 0;
+		if(room.getPlayers()[player].getNoPowers()) noPowerState = 1;
+		connection.query("UPDATE rankings SET points=points+?, noPower=noPower+?, nbPlayed=nbPlayed+1 WHERE member=?", room.getPlayers()[player].getPoints(), noPowerState, room.getPlayers()[player].getInfos().id, function(err, rows, fields) {
+			if (err) throw err;
+		}):
+		points.push({pseudo: room.getPlayers()[player].getInfos().pseudo, points: room.getPlayers()[player].getPoints()});	
 	}
-	roomRequest(room, 'stop', points);
+	connection.query("UPDATE rankings SET nbWon=nbWon+1 WHERE member=?", room.getPlayers()[player].getInfos().id, function(err, rows, fields) {
+		if (err) throw err;
+	}):
+	room.roomRequest('stop', points);
 }
 
-function generateBox(room, stepColor) {
+function generateBox(room, stepColor, power) {
 	var box = new boxes.Box(room.nextBox(), stepColor);
 	room.addBox(box);
-	roomRequest(room, 'box', {box: box});
+	box.setPower(power);
+	room.roomRequest('box', {box: box});
 	//console.log(box);
 }
 
-function roomRequest(room, type, data) {
-	for(index in room.getSockets()) {
-		room.getSockets()[index].emit(type, data);
+function generatePowers(room) {
+	var powers = room.getPowerList();
+	var selectedPowers = [];
+
+	var nbPowers = 3+Math.floor(Math.random() * 4);
+
+	for(i=0;i<nbPowers;i++) {
+		var chosenPower = Math.floor(Math.random() * powers.length);
+		selectedPowers.push(powers[chosenPower]);
 	}
+
+	return selectedPowers;
+}
+
+function compare(x, y) {
+	return x - y;
 }
